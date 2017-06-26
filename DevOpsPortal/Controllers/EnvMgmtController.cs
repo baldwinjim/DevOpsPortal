a45f70640f24896bc2dbf58d871f23c4d7ef09e9
Jim Baldwin <jbaldwin@worldventures.com>
2017-06-26 15:27:28 -0500

    initial commit

---
+++ DevOpsPortal/Controllers/EnvMgmtController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevOpsPortal.Controllers
+{
+    public class EnvMgmtController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
